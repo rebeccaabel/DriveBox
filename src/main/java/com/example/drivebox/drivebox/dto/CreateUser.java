a5d7e211a4b07c4514563f9c46042f389abb2769
rebeccaabel <rebeccaabel@hotmail.se>
2023-12-10 13:04:43 +0100

    created Update user DTO sp user can update email and username

--- src/main/java/com/example/drivebox/drivebox/dto/CreateUser.java
+++ src/main/java/com/example/drivebox/drivebox/dto/CreateUser.java
@@ -1,13 +1,23 @@
 package com.example.drivebox.drivebox.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CreateUser {
+    @NotBlank(message = "Username cannot be blank")
     private String username;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid Email")
     private String email;
 
 }
