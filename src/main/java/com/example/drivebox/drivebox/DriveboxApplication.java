60d4b6060bf5ca349313684288ee08142b3ade0d
rebeccaabel <rebeccaabel@hotmail.se>
2023-12-10 11:19:29 +0100

    initial commit

---
+++ src/main/java/com/example/drivebox/drivebox/DriveboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.drivebox.drivebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/drivebox/drivebox/DriveboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drivebox.drivebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
